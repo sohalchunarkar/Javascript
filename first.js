457b9e0572b14a56732803f434f3983966b2223a
sohal <sohalchunarkar8@gmail.com>
2026-02-16 16:18:02 +0530

    Started Javascript

---
+++ first.js
@@ -0,0 +1,18 @@
+console.log("Hello World")
+// let a = 5
+{
+    let a = 3
+    console.log(a)
+}
+a = 5
+console.log(a)
+let obj = {
+    "Name" : "Sohal",
+    "age" : 22,
+    "Bio" : false,
+    "salary" : undefined
+}
+console.log(obj["Name"])
+console.log(obj.Name)
+let c = BigInt("23423432")
+console.log(typeof c)
