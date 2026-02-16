da3c2d5df51024f5513e8b73f93acfd33061e223
sohal <sohalchunarkar8@gmail.com>
2026-02-17 01:26:25 +0530

    added comparison, datatypes etc

---
+++ 01_Basic.js
@@ -0,0 +1,32 @@
+//variables + data types + and 
+let var_1
+var_1 = "hello"
+let var_2 = "23"
+
+let var_3 = Number(var_2)
+// console.log(var_3 , typeof(var_3));
+// console.log(typeof(var_2))
+// console.log(var_2 == var_3);
+
+// console.log(typeof var_2 , typeof var_3)
+
+let number = 23
+
+let convertBoolean = Boolean(number)
+// console.log(convertBoolean);
+
+let someVar = "234sfd"
+let convertSomevar = Number(someVar)
+// console.log(convertSomevar)
+
+// console.log(typeof convertBoolean);
+
+
+
+// console.log(typeof var_1);
+
+//comparison
+
+console.log(var_1 != var_2);
+
+
