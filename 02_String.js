dbe5cb17604b194b3e467af40f51893b1e66ccdb
sohal <sohalchunarkar8@gmail.com>
2026-02-17 18:15:50 +0530

    Strings

---
+++ 02_String.js
@@ -0,0 +1,21 @@
+let name1 = "Alex"
+let name2 = "Name2"
+//string interpolation
+// console.log(`I am going to say my name is ${name1} and this is other one which is ${name2}`);
+
+const variable = new String("Hello this is string")
+
+console.log(variable[3]);
+// console.log(variable.charAt(3));
+// console.log(variable.indexOf('t'));
+console.log(variable.split(" "));
+const substring = variable.substring(0 ,5)
+console.log(substring);
+
+console.log(substring.includes("Hello"));
+
+
+
+const newStr= "     as     "
+console.log(newStr.trim());
+
