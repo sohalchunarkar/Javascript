0d079aad0bc93b4ec5701180bbb2677bc99374b4
sohal <sohalchunarkar8@gmail.com>
2026-02-18 19:19:38 +0530

    Function basics

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,20 @@
+// function first()
+// {
+//    return "This is the first ever function i have written"
+// }
+
+let str1
+// str1 = first()
+// console.log(str1);
+
+str1 = "user"
+
+function login(public = "bydefault person")
+{
+    return `${public} is logged in`
+}
+
+const ano = login()
+
+console.log(ano);
+
