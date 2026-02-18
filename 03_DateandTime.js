d83df014e9539b08894369ac6e1fc1762acc4010
sohal <sohalchunarkar8@gmail.com>
2026-02-18 13:16:47 +0530

    date

---
+++ 03_DateandTime.js
@@ -0,0 +1,13 @@
+let currDate = new Date(2003 , 7 , 3)
+console.log(currDate.toDateString());
+
+console.log(typeof currDate);
+
+
+let newDate = new Date()
+console.log(newDate.toDateString());
+console.log(newDate.getDay());
+
+
+
+
