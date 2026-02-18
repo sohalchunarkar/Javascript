b8466df9a9b232bb47489ea66d2b2136611b92bc
sohal <sohalchunarkar8@gmail.com>
2026-02-18 19:37:47 +0530

    array + obj handling + rest operaiont using ...

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -16,5 +16,43 @@ function login(public = "bydefault person")
 
 const ano = login()
 
-console.log(ano);
+// console.log(ano);
 
+
+//spread operation
+
+function takeMultiInput(...num)
+{
+    return num
+}
+
+//converts it into the array
+// console.log(takeMultiInput(100,200,300,400))
+
+
+const user ={
+    name : "ryan",
+    age : 18 ,
+    isVerified : true
+}
+
+function objectHandler(universalobj)
+{
+    return `${universalobj.name} is the name and age is ${universalobj.age} and the status is ${universalobj.isVerified}`
+}
+
+// console.log(objectHandler({
+//     name : "rohit",
+//     age : 23 ,
+//     isVerified : false
+// }));
+
+
+let array = [2,3,4,5,5]
+
+function arrayHandler(input)
+{
+    return input[0] + input[2]
+}
+
+console.log(arrayHandler(array));
