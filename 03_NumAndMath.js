da8670b971f5c9873dc379c1f7a4aac1ba49d1a0
sohal <sohalchunarkar8@gmail.com>
2026-02-18 12:35:04 +0530

    Num and math

---
+++ 03_NumAndMath.js
@@ -0,0 +1,23 @@
+// const num = 234.9324
+// console.log(num.toPrecision(3))
+
+const num = 234.934
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+
+ let num2 = 23424
+//  console.log(Math.sqrt(num2));
+
+// console.log(Math.random());
+
+//random generates number between 0 and 1
+console.log((Math.random()* 10) + 1 );
+
+// floored the value
+console.log(Math.floor((Math.random()* 10) + 1 ));
+
+let min = 20
+let max = 30
+console.log(Math.floor((Math.random()* (max - min ) + 1) + min ));
+
+
