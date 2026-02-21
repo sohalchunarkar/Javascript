38c36e661438d30a723db43635d62756f88ff852
sohal <sohalchunarkar8@gmail.com>
2026-02-21 15:45:31 +0530

    Swithc case

---
+++ 04_controlflow/02switch.js
@@ -0,0 +1,20 @@
+//switch syntx
+// if no case found then default case
+let a = 132
+switch (a) {
+    case 2:
+        console.log("this is 2");
+        break;
+
+    case 1:
+        console.log("this is 1");
+        break;
+
+    case 3:
+        console.log("this is 3");
+        break;
+
+    default:
+        console.log("this is defuault");
+        break;
+}
