aca36bda73570dcfe8e7b0505f417e1396072f30
sohal <sohalchunarkar8@gmail.com>
2026-02-21 15:42:09 +0530

    if else  + ternary operator  + logical operator + Nullish coalescing operator

---
+++ 04_controlflow/01if.js
@@ -0,0 +1,48 @@
+let isValid = true
+// if(isValid)
+// {
+//     console.log("this is true so executed")
+// }
+
+// if(!isValid){
+//     console.log("this is not true so not executed");
+    
+// }
+// else{
+//     console.log("this is  true so executed");
+
+// }
+
+// === checks data type also
+// ==
+// > , < , >= , <=  , !=
+let a = 1 
+let b = "1"
+if(a === b)
+{
+    console.log("these are equal");
+    
+}else{
+    console.log("these are not equal");
+
+}
+
+// if(1==1 && 2 == 2 )
+// {
+//     console.log("This is and operatior");
+    
+// }
+// if(1==1 || 2 == 1 )
+// {
+//     console.log("This is OR operatior");
+    
+// }
+
+//// Nullish Coalescing Operator (??): null undefined
+
+// let c = null ?? 123
+// console.log(c);
+
+//ternery operator
+let c = a ==b ? 12  : 13
+console.log(c);
