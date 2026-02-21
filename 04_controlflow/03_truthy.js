1ebb4590600509270291be142f15cd709824ea6a
sohal <sohalchunarkar8@gmail.com>
2026-02-21 15:46:56 +0530

    Truthy and falsy values

---
+++ 04_controlflow/03_truthy.js
@@ -0,0 +1,14 @@
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+let a = 0
+
+if(null)
+{
+    console.log("this is false");
+    
+}
